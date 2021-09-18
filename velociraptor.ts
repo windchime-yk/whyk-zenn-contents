f3f425fd56f4604603bc1672c6815764dfc9d230
WhyK <windchimeyk@gmail.com>
2021-09-18 23:19:40 +0900

    ci: add config file

---
+++ velociraptor.ts
@@ -0,0 +1,12 @@
+/**
+ * command managed by Velociraptor(https://velociraptor.run/)  
+ * ex. `vr preview`
+ */
+
+export default {
+  scripts: {
+    article: "npx zenn article",
+    book: "npx zenn book",
+    preview: "npx zenn preview",
+  },
+};
